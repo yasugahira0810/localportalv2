fed398668f8bc7f603e1c150219584346e854783
yasugahira0810 <hourou_freak@yahoo.co.jp>
2019-07-31 09:14:14 +0900

    Import layout to SiteList.vue

--- src/App.vue
+++ src/App.vue
@@ -18,13 +18,8 @@
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld
-  },
   data: () => ({
     //
   })
